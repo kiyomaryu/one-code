e16cde032fcafc7b4e1ad30fa3a01d6f3aada75c
kiyomaryu <ki4labo@gmail.com>
2023-01-19 09:16:15 +0900

    elixirのrestapiのサンプルを追加

---
+++ elixir/elixir_restapi/hello/config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :hello, Hello.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "hello_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :hello, HelloWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "LRJJ6wJF24hEmSpQA74h8+1llwL0KA4OGwFogNUTTBnaT2ls55jCs+nuAhhxBgOv",
+  server: false
+
+# In test we don't send emails.
+config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
---
+++ elixir/elixir_restapi/hello/lib/hello_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule HelloWeb.UserView do
+  use HelloWeb, :view
+  alias HelloWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{
+      id: user.id,
+      name: user.name,
+      age: user.age
+    }
+  end
+end
