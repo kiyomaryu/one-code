e1c0119fddd67d61d1b4ba761fa1e8bae7493a6f
きよまる <ki4labo@gmail.com>
2023-02-16 10:30:05 +0900

    Update release.exs

--- elixir/aws_lambda_container/config/release.exs
+++ elixir/aws_lambda_container/config/release.exs
@@ -1,6 +1,6 @@
 import Config
 
-config :ex_aws :s3,
+config :ex_aws, :s3,
   debug_requests: true,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
